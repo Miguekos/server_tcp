9bb915f5059bce92f23263253b1d747e13d86cea
Miguel Rodriguez <miguekos1233@gmail.com>
2020-12-11 21:58:06 -0500

    testtes

--- index.js
+++ index.js
@@ -25,17 +25,6 @@ server.on('connection', function (socket) {
 
     // The server can also receive data from the client by reading from its socket.
     socket.on('data', function (chunk) {
-        console.log(`Data received from client: ${chunk.toString()`.});
-    });
-
-    // When the client requests to end the TCP connection with the server, the server
-    // ends the connection.
-    socket.on('end', function() {
-        console.log('Closing connection with the client');
-    });
-
-    // Don't forget to catch error, for your own sake.
-    socket.on('error', function(err) {
-        console.log(`Error: ${ err }`);
+        console.log(`Data received from client: ${chunk.toString()`});
     });
 });
