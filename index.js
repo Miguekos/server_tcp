a5d6a07dc6982b4741d4460b99aa4563ef67a36d
Miguel Rodriguez <miguekos1233@gmail.com>
2020-12-11 21:51:59 -0500

    upad

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-// Include Nodejs' net module.
-const Net = require('net');
-// The port number and hostname of the server.
-const port = 4040;
-const host = '207.244.232.99';
+var net = require('net');
 
-// Create a new TCP client.
-const client = new Net.Socket();
-// Send a connection request to the server.
-client.connect({ port: port, host: host }), function () {
-    // If there is no error, the server has accepted the request and created a new 
-    // socket dedicated to us.
-    console.log('TCP connection established with the server.');
+var HOST = "0.0.0.0";
+var PORT = 4040;
 
-    // The client can now send data to the server by writing to its socket.
-    // client.write('Hello, server.');
-    client.on('data', function (chunk) {
+var server = net.createServer();
+server.listen(PORT, HOST);
+
+server.on('connection', function (sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    // sock.write("TCP sending message : 1");
+    console.log('Server listening on ' + server.address().address + ':' +
+        server.address().port);
+    server.on('data', function (chunk) {
         console.log(`Data received from the server: ${chunk.toString()}.`);
 
         // Request an end to the connection after the data has been received.
-        client.end();
+        server.end();
     });
-};
-
-// The client can also receive data from the server by reading from its socket.
-// client.on('data', function (chunk) {
-//     console.log(`Data received from the server: ${chunk.toString()}.`);
-
-//     // Request an end to the connection after the data has been received.
-//     client.end();
-// });
-
-// client.on('end', function () {
-//     console.log('Requested an end to the TCP connection');
-// });
+}).listen(PORT, HOST);
